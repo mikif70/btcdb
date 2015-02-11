7a3859bebf04493d85b5c27aaf201beb07b009ce
Michele Fadda <mikif70@gmail.com>
2015-02-11 13:21:50 +0100

    mongodb insert

--- blockRead.go
+++ blockRead.go
@@ -3,14 +3,22 @@ package main
 import (
 	//	"encoding/json"
 	"fmt"
+	"gopkg.in/mgo.v2"
 )
 
 func main() {
+
+	session, err := mgo.Dial("10.39.81.90")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
 
 	paramsInt := make([]int, 0)
 
 	var blockCount = Call("getblockcount", paramsInt)
 
+	db := session.DB("btc").C("block")
 	paramsInt = make([]int, 1)
 	paramsString := make([]string, 1)
 	for i := 0.0; i < blockCount.(float64); i++ {
@@ -18,6 +26,12 @@ func main() {
 		var hash = Call("getblockhash", paramsInt)
 		paramsString[0] = hash.(string)
 		var block = Call("getblock", paramsString)
-		fmt.Println(block.(map[string]interface{}))
+		//		fmt.Println(block.(map[string]interface{}))
+		err = db.Insert(block.(map[string]interface{}))
+		if err != nil {
+			fmt.Println(i)
+		} else {
+			fmt.Print(".")
+		}
 	}
 }
